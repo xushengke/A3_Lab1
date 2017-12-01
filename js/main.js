0c8f4fa682b5b9b856b7007b74765dff03445b47
Xu <s_xu9@fanshawec.ca>
2017-12-01 11:54:12 -0500

    thisis a class file

--- js/main.js
+++ js/main.js
@@ -6,19 +6,63 @@
        modelInfo = document.querySelector('.modelDetails'),
        appliedClass;
 
-       function changeElements() {
+       const httpRequest = new XMLHttpRequest();
+       function getCarData() {
+          //set up the AJAX call => handle errors first
+          if (!httpRequest) {
+            alert('giving up, your browser sucks');
+            return false;
+          }
+
+          httpRequest.onreadystatechange = processRequest;
+          httpRequest.open('GET', './includes/functions.php?carModel=' + this.id);
+          httpRequest.send();
+          }
+
+
+        function processRequest() {
+          let reqIndicator = document.querySelector('.request-state');
+          reqIndicator.textContent = httpRequest.readyState;
+          debugger;
+
+          if (httpRequest.readyState === XMLHttpRequest.DONE) {
+            if (httpRequest.status === 200) { // 200 means everything is awesome
+              //debugger;
+              let data = JSON.parse(httpRequest.responseText); //turn data into js project
+
+              changeElements(data);
+            } else {
+              alert('There was a problem with the request.');
+            }
+          }
+        }
+
+
+
+
+
+
+
+       function changeElements(data) {
+         const { modelname, pricing, modelDetails} = data;
          let carIndex = carData[this.id];
+        let modelName = document.querySelector('.modelName').textContent = modelname;
+        let priceInfo = document.querySelector('.priceInfo').innerHTML = pricing;
+        let modelInfo = document.querySelector('.modelDetails').textContent = modelDetails;
+
+         //modelName.firstChild.nodeValue = carIndex.carName;
+        // priceInfo.firstChild.nodeValue = carIndex.price;
+        // modelInfo.innerHTML = carIndex.description;
 
 
-         modelName.firstChild.nodeValue = carIndex.carName;
-         priceInfo.firstChild.nodeValue = carIndex.price;
-         modelInfo.innerHTML = carIndex.description;
 
          carImages.forEach(function(element, index){
            element.classList.add('nonActive');
-         })
+         });
 
-         this.classList.remove('nonActive');
+
+          document.querySelector('#${data.model}').classList.remove('nonActive');
+         //this.classList.remove('nonActive');
 
 
 
@@ -29,7 +73,7 @@
 
        carImages.forEach(function(element, index) {
     // loop through the images and add event handling to each one
-    element.addEventListener('click', changeElements, false);
+    element.addEventListener('click', getCarData, false);
   });
 
 
