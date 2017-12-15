797c363e68add07203276ced8fb3eab130e2dc8b
Xu <s_xu9@fanshawec.ca>
2017-12-15 11:39:46 -0500

    finish video stuff

---
+++ js/modules/video.js
@@ -0,0 +1,86 @@
+var video = {
+  //variables at the top
+  videoPlayer : document.querySelector('video'),
+  vidThumb : document.querySelectorAll('.vid-thumb'),
+  volumeIndicator : document.querySelector('.vol-indicator'),
+
+  volOn() {
+    video.videoPlayer.muted = false;
+    video.volumeIndicator.classList.replace('fa-volume-off', 'fa-volume-up');
+  },
+
+  volOff() {
+    video.videoPlayer.muted = true;
+    video.volumeIndicator.classList.replace('fa-volume-up', 'fa-volume-off');
+  },
+
+popOverlay() {
+  let overlay = document.querySelector('.vid-overlay');
+  overlay.classList.add('show-overlay');
+  overlay.querySelector('i').addEventListener('click', video.replayVideo);
+},
+
+replayVideo() {
+  //debugger;     rewind the video
+  video.videoPlayer.currentTime = 0;
+  video.videoPlayer.play();
+
+  let overlay = document.querySelector('.vid-overlay');
+  overlay.classList.remove('show-overlay');
+  overlay.querySelector('i').addEventListener('click', video.replayVideo);
+},
+
+  fetchVideoThumbs() {
+    //debugger;
+    const url = './includes/functions.php?getVideos=true';
+
+    fetch(url)// make an ajax call for the video thumbs
+      .then((resp) => resp.json())//connect result to json
+      .then((data) => { video.loadVideoThumbs(data); })
+      .catch(function(error) {
+          console.log(error);
+      });
+
+  },
+
+  loadVideoThumbs(data) {
+    let thumbHolder = document.querySelector('.video-thumbs');
+
+    data.forEach(thumb => {
+      // create an li element for each new vid
+      let docFrag =
+      `<li class="vid-thumb" role="button" data-videopath="${thumb.path}">
+        <img src="images/${thumb.placeholder}" alt="mini commercial" class="responsive">
+      </li>`;
+
+      thumbHolder.innerHTML += docFrag;
+      thumbHolder.querySelectorAll('li').forEach((thumb) => thumb.addEventListener('click', video.loadNewVideo));
+    });
+  },
+
+  loadNewVideo() {
+    let videoPath = "video/" + this.dataset.videopath;
+
+    video.videoPlayer.src = videoPath;
+    video.videoPlayer.load();
+    video.videoPlayer.play();
+
+    let overlay = document.querySelector('.vid-overlay');
+    overlay.classList.remove('show-overlay');
+
+    video.volOn();
+  },
+
+  //then functionality
+  init() {
+    console.log('video module added');
+
+    video.videoPlayer.addEventListener('mouseover', video.volOn);
+    video.videoPlayer.addEventListener('mouseout', video.volOff);
+    video.videoPlayer.addEventListener('ended', video.popOverlay);
+
+    video.fetchVideoThumbs();
+  }
+}
+
+video.init();
